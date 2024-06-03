e7ed616ccb3e2b43704bc9b08e977b0f28747678
anajesset <anajesset1@gmail.com>
2024-06-03 22:53:52 +0800

    update lupa

--- consultation.py
+++ consultation.py
@@ -46,3 +46,4 @@ def consultation():
 
 if __name__ == "__main__":
     consultation()
+    
--- user.py
+++ user.py
@@ -5,10 +5,10 @@ import hashlib
 
 def create_connection():
     conn = mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="",
-        database="patient_database"
+        host="sql.freedb.tech",
+        user="freedb_anajesset",
+        password="D9@WjYGE9?6&b&n",
+        database="freedb_scpk_database"
     )
     return conn
 
